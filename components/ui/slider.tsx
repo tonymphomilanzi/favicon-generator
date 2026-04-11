b7c5d0638100aa2842def6aec6a21bab814aa6ab
Tony Milanzi <tonymilanzi01@gmail.com>
2026-04-11 15:19:53 +0200

    Fixed Slider component not showing error

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { Slider as SliderPrimitive } from "radix-ui"
-
 import { cn } from "@/lib/utils"
 
 function Slider({
@@ -11,6 +10,7 @@ function Slider({
   value,
   min = 0,
   max = 100,
+  orientation = "horizontal",
   ...props
 }: React.ComponentProps<typeof SliderPrimitive.Root>) {
   const _values = React.useMemo(
@@ -30,26 +30,36 @@ function Slider({
       value={value}
       min={min}
       max={max}
+      orientation={orientation}
       className={cn(
-        "relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col",
+        "relative flex w-full touch-none items-center select-none data-disabled:opacity-50",
+        orientation === "vertical"
+          ? "h-full min-h-40 w-auto flex-col"
+          : "flex-row",
         className
       )}
       {...props}
     >
       <SliderPrimitive.Track
         data-slot="slider-track"
-        className="relative grow overflow-hidden rounded-full bg-muted data-horizontal:h-1 data-horizontal:w-full data-vertical:h-full data-vertical:w-1"
+        className={cn(
+          "relative grow overflow-hidden rounded-full bg-zinc-200",
+          orientation === "vertical" ? "h-full w-1.5" : "h-1.5 w-full"
+        )}
       >
         <SliderPrimitive.Range
           data-slot="slider-range"
-          className="absolute bg-primary select-none data-horizontal:h-full data-vertical:w-full"
+          className={cn(
+            "absolute bg-primary",
+            orientation === "vertical" ? "w-full" : "h-full"
+          )}
         />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="relative block size-3 shrink-0 rounded-full border border-ring bg-white ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 disabled:pointer-events-none disabled:opacity-50"
+          className="relative block size-4 shrink-0 rounded-full border border-zinc-300 bg-white shadow-md ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-2 hover:ring-ring/50 focus-visible:ring-2 focus-visible:ring-ring focus-visible:outline-none active:ring-2 disabled:pointer-events-none disabled:opacity-50"
         />
       ))}
     </SliderPrimitive.Root>
